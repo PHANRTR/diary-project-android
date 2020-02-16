497801e02d6fe69ab3644b040b2af947a231a63c
Paulo <paulonunes@natura.net>
2020-02-16 09:24:10 -0300

    Creating layout and list of students

--- app/src/main/java/com/br/agenda/MainActivity.java
+++ app/src/main/java/com/br/agenda/MainActivity.java
@@ -2,18 +2,27 @@ package com.br.agenda;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-        TextView aluno = new TextView(this);
-        aluno.setText("Meu app");
-        setContentView(aluno);
+        setContentView(R.layout.activity_main);
+
+        List<String> students = new ArrayList<>(Arrays.asList("PH", "Ana", "Mariano", "Bianca"));
+
+        ListView listStudents = findViewById(R.id.activity_main_list_of_students);
+
+        listStudents.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, students));
     }
 }
